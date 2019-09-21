301542fd9cdc41b4ace1fe2ea5538fd20fe4bce0
Emily_Richard <richard.emily.e@gmail.com>
2019-09-21 19:16:08 -0400

    Added styling to Login.js, added images folder in /src/

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import axiosWithAuth from '../utils/axiosWithAuth';
+import styled from 'styled-components';
+import './Login.css';
 
 const Login = (props) => {
   const [credentials, setCredentials] = useState({username: "", password: ""});
@@ -7,6 +9,49 @@ const Login = (props) => {
   const handleChange = e => {
     setCredentials({...credentials, [e.target.name]: e.target.value});
   };
+
+  const Header = styled.div`
+    color: white;
+    font-size: 3rem;
+    width: 100%;
+    height: 10vh;
+    padding: 2.5% 50px;
+    background-image: linear-gradient(to right, #565f64, #eeeff3);
+  `;
+
+  const Login = styled.div`
+    position: absolute;
+    right: 200px;
+    top: 20%;
+    width: 40%;
+    max-width: 500px;
+    background-color: white;
+    border-radius: 20px;
+    box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24);
+  `;
+
+  const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    padding: 50px 0 30px;
+    width: 100%;
+  `;
+
+  const Input  = styled.input`
+    padding: 5px 10px;
+    margin: 10px auto;
+    width: 50%;
+    border-radius: 10px;
+    box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24);
+  `;
+
+  const Button = styled.button`
+    padding: 5px 40px;
+    margin: 10px auto;
+    border-radius: 10px;
+    box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24);
+  `;
+
 
 //   const login = e => {
 //     e.preventDefault();
@@ -23,23 +68,26 @@ const Login = (props) => {
 
   return (
     <>
-      <form>
-        <input
-        type="text"
-        name="username"
-        placeholder = "User Name"
-        value={credentials.username}
-        onChange={handleChange}
-        />
-        <input
-        type="password"
-        name="password"
-        placeholder = "Password"
-        value={credentials.password}
-        onChange={handleChange}
-        />
-        <button>Log in</button>
-    </form>
+      <Header>Wanderlust</Header>
+      <Login>
+        <Form>
+          <Input
+          type="text"
+          name="username"
+          placeholder = "User Name"
+          value={credentials.username}
+          onChange={handleChange}
+          />
+          <Input
+          type="password"
+          name="password"
+          placeholder = "Password"
+          value={credentials.password}
+          onChange={handleChange}
+          />
+          <Button>Log in</Button>
+        </Form>
+      </Login>
     </>
   );
 };
