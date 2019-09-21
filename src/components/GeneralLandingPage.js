12809113eb6d85fcba76a69ee38832cc5d548f5b
Brandon-Harris <brandonharris177@gmail.com>
2019-09-21 17:09:35 -0600

    links added

--- src/components/GeneralLandingPage.js
+++ src/components/GeneralLandingPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
 const GeneralLandingPage = (props) => {
 
@@ -7,6 +8,8 @@ const GeneralLandingPage = (props) => {
     <>
     General Landing Page 
     -this page will have a blurb  about us and links to the User Browsing page and the Creator Viewing pages
+    <Link to = "/user-browsing-page">User</Link>
+    <Link to = "/creator-viewing-page">Creator</Link>
     </>
   );
 };
