4a33c9ecc76a8832a1293fd07db69a62a5d9fb96
Greg <gsc229@gmail.com>
2019-09-23 15:25:48 -0400

    Links hooked up not styled yet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // import PrivateRoute from './components/PrivateRoute';
-import GeneralLandingPage from './components/GeneralLandingPage';
-import UserBrowsingPage from './components/Users/UserBrowsing';
-import CreatorViewingPage from './components/Creators/CreatorViewingPage';
-import CreatorCreateExperienceForm from './components/Creators/CreatorCreateExperience';
-import CreatorUpdateExperienceForm from './components/Creators/CeatorUpdateExperience'
+import GeneralLandingPage from "./components/GeneralLandingPage";
+import UserBrowsingPage from "./components/Users/UserBrowsing";
+import CreatorViewingPage from "./components/Creators/CreatorViewingPage";
+import CreatorCreateExperienceForm from "./components/Creators/CreatorCreateExperience";
+import CreatorUpdateExperienceForm from "./components/Creators/CeatorUpdateExperience";
 
 import Login from "./components/Login";
 
@@ -16,11 +16,27 @@ function App() {
       <div className="App">
         <Route exact path="/" component={Login} />
         {/* <PrivateRoute exact path="/general-landing-page" component={GeneralLandingPage}/> */}
-        <Route exact path= "/general-landing-page" component={GeneralLandingPage}/>
-        <Route exact path= "/user-browsing-page" component={UserBrowsingPage} />
-        <Route exact path = "/creator-viewing-page" component = {CreatorViewingPage} />
-        <Route exact path = "/creator-create-experiance-form" component = {CreatorCreateExperienceForm} />
-        <Route exact path = "/creator-update-experiance-form" component = {CreatorUpdateExperienceForm} />
+        <Route
+          exact
+          path="/general-landing-page"
+          component={GeneralLandingPage}
+        />
+        <Route exact path="/user-browsing-page" component={UserBrowsingPage} />
+        <Route
+          exact
+          path="/creator-viewing-page"
+          component={CreatorViewingPage}
+        />
+        <Route
+          exact
+          path="/creator-create-experiance-form"
+          component={CreatorCreateExperienceForm}
+        />
+        <Route
+          exact
+          path="/creator-update-experiance-form"
+          component={CreatorUpdateExperienceForm}
+        />
       </div>
     </Router>
   );
--- src/components/GeneralLandingPage.js
+++ src/components/GeneralLandingPage.js
@@ -1,12 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import "./GeneralLandingPage.css";
+import { Link } from "react-router-dom";
 
-const GeneralLandingPage = (props) => {
+const Header = styled.div`
+  color: white;
+  font-size: 3rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  height: 10vh;
+  padding: 2.5% 50px;
+  background-image: linear-gradient(to right, #565f64, #eeeff3);
+`;
 
+const Button = styled.button`
+  padding: 5px 40px;
+  margin: 10px auto;
+  border-radius: 10px;
+  box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12),
+    0 2px 4px rgba(0, 0, 0, 0.24);
+`;
+
+const GeneralLanding = styled.div`
+  border: 2px solid yellow;
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const GeneralLandingPage = props => {
+  const [experiences, setExperiences] = useState([]);
+  const routeToUserBrowsing = event => {
+    event.preventDefault();
+    props.history.push("/user-browsing-page");
+  };
+  const routeToCreateExp = event => {
+    event.preventDefault();
+    props.history.push("/creator-create-experiance-form");
+  };
+  /* https://wanderlustbw.herokuapp.com/experiences */
+  useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/character/`).then(response => {
+      console.log("response", response.data.results);
+    });
+  }, []);
 
   return (
     <>
-    General Landing Page 
-    -this page will have a blurb  about us and links to the User Browsing page and the Creator Viewing pages
+      <Header>
+        Wanderlust
+        <nav className="gerneral-header-nav">
+          <Link to="/creator-update-experiance-form">Trip History</Link>
+          <Link to="/creator-viewing-page">Created Trips</Link>
+        </nav>
+      </Header>
+      <GeneralLanding>
+        <h1>HI FROM GENERAL LANDING PAGE</h1>
+        <div className="create-or-find">
+          <div className="find-experience">
+            <h2>Featured Experiences:</h2>
+            <div>
+              <ul>
+                <li>Expericne One</li>
+                <li>Experience Two</li>
+                <li>Experience Three</li>
+              </ul>
+            </div>
+            <Button onClick={routeToUserBrowsing}>Serch for more trips</Button>
+          </div>
+          <div className="create-own-experience">
+            <h2>Create Your Own Experiecne</h2>
+            <Button onClick={routeToCreateExp}>Create</Button>
+          </div>
+        </div>
+      </GeneralLanding>
     </>
   );
 };
