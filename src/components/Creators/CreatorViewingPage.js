02b43983ad744322c7a0b61e60d3e4676063d5ef
Emily_Richard <richard.emily.e@gmail.com>
2019-09-23 14:05:58 -0400

    Structured cards, linked to CreatorViewingPage

--- src/components/Creators/CreatorViewingPage.js
+++ src/components/Creators/CreatorViewingPage.js
@@ -3,16 +3,34 @@ import axios from 'axios';
 import styled from 'styled-components';
 
 import Header from '../Header';
+import Card from './CreatorViewingCards';
 
 const CreatorViewingPage = (props) => {
 
-  const api = "https://wanderlustbw.herokuapp.com/experiences/organizer/name/test1";
+  const user = "";
+  const api = `https://wanderlustbw.herokuapp.com/experiences/organizer/name/${user}`;
 
-  const {experiences, setExperiences} = useState([{
+  const [experiences, setExperiences] = useState([{
     id: 1,
     name: "Preview Experience",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    date: "09/23/2019",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    date: "09/23/2019 1PM",
+    duration: 1,
+    location_id: 1,
+    completed: false
+  }, {
+    id: 2,
+    name: "Preview Experience",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    date: "09/23/2019 2PM",
+    duration: 1,
+    location_id: 1,
+    completed: false
+  }, {
+    id: 2,
+    name: "Preview Experience",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    date: "09/23/2019 3PM",
     duration: 1,
     location_id: 1,
     completed: false
@@ -22,6 +40,7 @@ const CreatorViewingPage = (props) => {
     axios.get(api)
     .then(response => {
       console.log(response)
+      setExperiences(...experiences, response)
     })
     .catch(error => {
       console.log(error)
@@ -37,29 +56,28 @@ const CreatorViewingPage = (props) => {
   border-radius: 10px;
   box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24);
 `;
-const Experience = styled.div`
-  background-color: white;
-  width: 50%;
-  max-width: 750px;
-  margin: 5% auto;
-  padding: 30px 40px;
-  border-radius: 10px;
-  box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24);
-`;
 
   return (
     <>
-    <Header />
-    <ExperienceList>
-      <h2>Experiences You're Hosting</h2>
-    </ExperienceList>
-    {/* {
-      experiences.map((experience) => {
-        return (
-          <ExperienceList>Experience</ExperienceList>
-          )
-      })
-    } */}
+      <Header />
+      <ExperienceList>
+        <h2>Experiences You're Hosting</h2>
+      </ExperienceList>
+        {
+          experiences.map((experience) => {
+            return (
+              <Card
+                key={experience.id}
+                name={experience.name}
+                description={experience.description}
+                date={experience.date}
+                duration={experience.duration}
+                location_id={experience.location_id}
+                completed={experience.completed}
+                />
+              )
+          })
+        }
     </>
   );
 };
