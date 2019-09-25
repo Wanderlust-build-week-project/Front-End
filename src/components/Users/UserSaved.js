8282c3e64e8fcd2109babb13fd7ce250917922fe
CScori <amilanotte1366@gmail.com>
2019-09-25 10:04:17 -0500

    adds saved expr card

--- src/components/Users/UserSaved.js
+++ src/components/Users/UserSaved.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const UserSaved = (props) => {
     return (
         <div>
-           <h3>Saved Experiences</h3> 
-           {props.list.map(thing => (
-               <span>{thing.title}</span>
-           ))}
+            <h3>Saved Experiences</h3>
+            {props.list.map(thing => (
+                <span>{thing.title}</span>
+            ))}
+            <Link to='/'>
+                <button>Save More Trips!</button>
+            </Link>
         </div>
+
     )
 }
 
