e7f74f4f0465b425590e9f3686c9f30001accc10
CScori <amilanotte1366@gmail.com>
2019-09-25 08:50:42 -0500

    adds experience card for saved

--- src/components/Users/Experience.js
+++ src/components/Users/Experience.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axiosWithAuth from '../../utils/axiosWithAuth';
+import {
+    Card, CardImg, CardText, CardBody, CardLink,
+    CardTitle, CardSubtitle, Row, Col
+} from 'reactstrap';
 
 const Experience = () => {
+const [exprnc, setExprnc] = useState({})
+
+useEffect(() => {
+    const id = ;
+    //need to set params from call 
+    axiosWithAuth
+    .get()
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => console.log(err)
+    )
+
+}, [id.param])
+
+const saveExpr = () => {
+    const addToSaved = props.addToSaved;
+    addToSaved(exprnc)
+}
+
+
+
     return (
         <div>
-            
+            <Header />
+             <Row>
+                <Col sm="6">
+                    <Card>
+                        <CardBody {...props.key}>
+                            <CardTitle>{props.title}</CardTitle>
+                            <CardSubtitle>{props.desc}</CardSubtitle>
+                            <CardText>{props.hours}</CardText>
+                            <CardText>{props.date}</CardText>
+                        </CardBody>
+                        <CardImg src={props.img} alt={props.title}/>
+                        <div onClick={props.exprnc}>Save This Trip</div>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
